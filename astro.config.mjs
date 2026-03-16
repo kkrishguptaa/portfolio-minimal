ae789bcfab1288c0178049b0b89557147cda47b4
Krish Gupta <m.krishggupta@icloud.com>
2026-03-16 14:33:06 +0530

    fix: Set base path for Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap(), react()],
+  base: '/portfolio-minimal',
   vite: {
     plugins: [tailwindcss()],
   },
